3c77578ef59479bfddbc0bca5392b810142669d5
Mina Nagy Zaki <mnzaki@gmail.com>
2017-12-14 03:26:55 +0200

    sketch1

--- src/sketches/sketch1.js
+++ src/sketches/sketch1.js
@@ -7,17 +7,17 @@ export default p => {
   });
 
   p.setup = () => {
-    p.createLiveCanvas(700, 500);
+    p.createLiveCanvas(700, 400);
     p.fill(30);
   };
 
   p.draw = () => {
-    p.background(100, 100, 10, 20);
-    p.ellipse(p.live.x, p.live.y*20, 30, 70);
-    p.ellipse(p.live.x, p.live.y*p.random(40), 30, 70);
-    p.ellipse(p.live.x*4, p.live.y*20, 30, 70);
+    p.background(150, 100, 10, 20);
+    p.ellipse(p.live.x, p.live.y, 10, 7);
+    p.ellipse(p.live.x, p.live.y*p.random(40), 10, 70);
+    p.ellipse(p.live.x*4, p.live.y, 30, 70);
     p.live.x += delta;
-    p.fill(p.random(255));
+    p.fill(p.random(255), 100, 100);
     if (p.live.x > p.width) delta = -p.abs(delta);
     if (p.live.x < 0) delta = p.abs(delta);
   }
